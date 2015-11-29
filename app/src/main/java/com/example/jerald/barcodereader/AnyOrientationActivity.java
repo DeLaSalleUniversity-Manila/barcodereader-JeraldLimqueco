5813b19b3797570b4bf0f2c514fce28559eaf5d6
Jerald Limqueco <jeraldlimqueco@yahoo.com>
2015-11-30 02:29:47 +0800

    barcode reader

---
+++ app/src/main/java/com/example/jerald/barcodereader/AnyOrientationActivity.java
@@ -0,0 +1,14 @@
+package com.example.jerald.barcodereader;
+
+/**
+ * Created by Jerald on 11/30/2015.
+ */
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * This Activity is exactly the same as CaptureActivity, but has a different orientation
+ * setting in AndroidManifest.xml.
+ */
+public class AnyOrientationActivity extends CaptureActivity {
+
+}
